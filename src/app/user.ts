ba354468c8f9c5e72b6cc8aa453eb6f915ffb291
nealwaga <waganeal@gmail.com>
2022-04-16 01:41:24 +0300

    user class changes

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,24 @@
 export class User {
+    constructor (
+        public name:string,
+        public public_repo:number,
+        public avatar_url: string,
+        public login:string,
+        public company:string,
+        public created_at:Date,
+        public following:number,
+        public followers:number,
+        public html_url:string
+      ) {
+        this.name = name;
+        this.public_repo = public_repo;
+        this.avatar_url = avatar_url;
+        this.login = login;
+        this.company = company;
+        this.created_at = created_at;
+        this.following = following;
+        this.followers = followers;
+        this.html_url = html_url;
+    
+      }
 }
