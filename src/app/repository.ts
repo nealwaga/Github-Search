3019791cc6819f27a860aae492414f20efe1cff2
nealwaga <waganeal@gmail.com>
2022-04-16 01:43:43 +0300

    repository class changes

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,19 @@
 export class Repository {
+
+    constructor(
+        public name:string,
+        public description: string,
+        public forks: number,
+        public html_url: string,
+        public created_at : Date
+    
+      ) {
+        this.name = name;
+        this.description = description;
+        this.forks = forks;
+        this.html_url = html_url;
+        this.created_at = created_at;
+    
+      }
+      
 }
