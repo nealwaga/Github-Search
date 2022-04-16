27bf0da856fc16a9bff19b1269d49439fc00733e
nealwaga <waganeal@gmail.com>
2022-04-16 10:13:34 +0300

    user service input

---
+++ src/app/user-service/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+
+import { from } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private userName:string;
+
+
+  constructor(private http:HttpClient) {
+    console.log("Service is ready");
+    this.userName = "melissakobia";
+   }
+
+
+  getUserInfo() {
+    return this.http.get("https://api.github.com/users/" + this.userName + "?clientId=" + environment.clientId
+     + "&clientSecret=" +environment.clientSecret);
+  }
+
+
+}
