20e1eff7278e97f1a6a9799905da2f6d0330d2b0
nealwaga <waganeal@gmail.com>
2022-04-16 15:46:58 +0300

    main component addition

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+//import { User } from '../user';
+import { UserService } from '../user-service/user.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+//import { RepositoryService } from '../repository-service/repository-service.service';
+
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
+
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+    //users:User;
 
-  ngOnInit(): void {
-  }
+    profile: any;
+    repos: any;
+    userName: string;
+  
+  
+  
+    constructor(
+      private userService: UserService, private repositoryService: RepositoryService
+  
+    ) {
+  
+    }
+  
+  
+    findUser() {
+      this.userService.updateUser(this.userName);
+      this.userService.getUserInfo()
+      this.profile = this.userService.profile;
+  
+      this.repositoryService.updateUser(this.userName);
+      this.repositoryService.getRepoInformation()
+      this.repos = this.repositoryService.repos;
+    }
+  
+  
+  
+  
+    ngOnInit() {
+    }
 
 }
