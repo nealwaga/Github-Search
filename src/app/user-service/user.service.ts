e86c792f6d8313d963417702a8647651b0ca78a3
nealwaga <waganeal@gmail.com>
2022-04-16 10:20:02 +0300

    new function addition

--- src/app/user-service/user.service.ts
+++ src/app/user-service/user.service.ts
@@ -18,14 +18,31 @@ export class UserService {
     this.userName = "melissakobia";
    }
 
-
+// function getting user info using API
   getUserInfo() {
     return this.http.get("https://api.github.com/users/" + this.userName + "?clientId=" + environment.clientId
      + "&clientSecret=" +environment.clientSecret);
   }
 
+// function getting user repos  
   getRepoInfo() {
     return this.http.get("https://api.github.com/users/" + this.userName + "/repos?clientId=" + environment.clientId + "&clientSecret=" +environment.clientSecret);
+  }
+
+   // getUserInfo() {
+  //   return this.http.get("https://api.github.com/repos/users/"+this.userName+"/issues?state=closed&access_token=945f965fc6d9c2c28798a70431e7dc37121d321f");
+  // }
+
+  // getRepoInfo(){
+  //   return this.http.get("https://api.github.com/repos/users/"+this.userName+"/repos/issues?state=closed&access_token=945f965fc6d9c2c28798a70431e7dc37121d321f");
+  // }
+
+
+// function updating the username when searching
+  updateUser (userName : string) {
+    this.userName = userName;
+
+
   }
 
 
