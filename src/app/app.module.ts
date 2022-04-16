194d7c537c303621ff728a8d3d51fcfa6435b639
nealwaga <waganeal@gmail.com>
2022-04-16 14:53:23 +0300

    generation of pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { RepositoryComponent } from './repository/repository.component';
+import { DatePipe } from './date.pipe';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RepositoryComponent } from './repository/repository.component';
     NavbarComponent,
     MainPageComponent,
     RepositoryComponent,
+    DatePipe,
   ],
   imports: [
     BrowserModule,
